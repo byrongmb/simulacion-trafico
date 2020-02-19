c3780e69814cecc93a25aca6d9220b91c1fc155d
byrongamboa <byron.gamboab@ug.edu.ec>
2020-02-18 23:53:16 -0500

    Agg imagen

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,17 +1,35 @@
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class Vehiculo {
+
+    private JLabel car;
     private int posX;
     private int posY;
-    private String imagenUrl;
+    private String imagenUrl = "/img/car_red.png";
 
-    public Vehiculo() {
+    public Vehiculo(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+        car = new JLabel(new ImageIcon(imagenUrl));
+    }
 
+    public void moverX(){
+        posX += 5;
+    }
+    public void moverY()
+    {
+        posY += 5;
+    }
+
+    //Getters and Setters
+    public JLabel getCar(){
+        return this.car;
     }
 
     public int getPosY() {
         return posY;
     }
-
     public void setPosY(int posY) {
         this.posY = posY;
     }
@@ -19,7 +37,6 @@ public class Vehiculo {
     public int getPosX() {
         return posX;
     }
-
     public void setPosX(int posX) {
         this.posX = posX;
     }
