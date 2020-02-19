d43d84b035436b5ce079a77bb1be718b06e5aa97
byrongamboa <byron.gamboab@ug.edu.ec>
2020-02-18 23:23:28 -0500

    Creacion Clase Vehiculo

---
+++ Vehiculo.java
@@ -0,0 +1,27 @@
+
+public class Vehiculo {
+    private int posX;
+    private int posY;
+    private String imagenUrl;
+
+    public Vehiculo() {
+
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+}
